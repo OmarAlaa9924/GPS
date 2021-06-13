8f57472cae6f6bae17b9dcda2c330b33142046e2
Ahmed7320 <85245233+Ahmed7320@users.noreply.github.com>
2021-06-13 23:36:59 +0200

    Add files via upload

---
+++ lcd.d
@@ -0,0 +1,4 @@
+.\lcd.o: LCD.c
+.\lcd.o: Initializations.h
+.\lcd.o: tm4c123gh6pm.h
+.\lcd.o: Lcd.h
---
+++ main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: Initializations.h
+.\main.o: tm4c123gh6pm.h
+.\main.o: Lcd.h
